6d5b8c2a6101b5468ccd7259a33b85c6fb3371fd
luishads <luishads@gmail.com>
2021-06-22 14:05:17 -0300

    new branch continuation, with corrections in some exercises

--- C05/ex01/ft_recursive_factorial.c
+++ C05/ex01/ft_recursive_factorial.c
@@ -5,5 +5,5 @@ int	ft_recursive_factorial(int nb)
 	if (nb == 0)
 		return (1);
 	else
-	return (nb * ft_recursive_factorial(nb - 1));
+		return (nb * ft_recursive_factorial(nb - 1));
 }
--- C05/ex02/ft_iterative_power.c
+++ C05/ex02/ft_iterative_power.c
@@ -1,20 +1,18 @@
 int	ft_iterative_power(int nb, int power)
 {
-	int	sign;
+	int	result;
 
-	sign = 1;
 	if (power < 0)
 		return (0);
 	if (power == 0 || (nb == 0 && power == 0))
 		return (1);
-	if (nb < 0 && power % 2 != 0)
-		sign = sign * -1;
 	if (power == 1)
 		return (nb);
+	result = nb;
 	while (power > 1)
 	{
-		nb *= nb;
+		result *= nb;
 		power--;
 	}
-	return (nb * sign);
+	return (result);
 }
